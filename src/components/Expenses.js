5b208f59a651a727260fcd26b819ea87419ff55a
heorhi.puhachou <h.puhachou@gmail.com>
2021-12-20 17:41:37 +0300

    Changes

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from './Card';
+
+function Expenses(props) {
+  const expenses = props.expenses
+    .map(expense => <ExpenseItem title={expense.title}
+                                 amount={expense.amount}
+                                 date={expense.date} />);
+  return (<Card className="expenses">{expenses}</Card>);
+
+}
+
+export default Expenses;
